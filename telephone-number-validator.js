e8508bacc8cebd74a4c834b506812c045a08ec74
nstelmach <90278376+nstelmach@users.noreply.github.com>
2022-10-22 21:50:09 +0200

    Update telephone-number-validator.js

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -1,11 +1,6 @@
 function telephoneCheck(str) {
-  let newStr = str.replace(/\s+/g, '');
+  let newStr = str.replace(/\s+/g, "");
   let firstOpt = /^1?\(\d{3}\)\d{3}-?\d{4}$/.test(newStr);
   let secondOpt = /^1?\d{3}-?\d{3}-?\d{4}$/.test(newStr);
-  if (firstOpt || secondOpt) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  return firstOpt || secondOpt;
 }
